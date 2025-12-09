4729420273b8e1d8041a5c8c857ad6ac65c094d4
hanyong5 <hanyong5@naver.com>
2025-12-09 12:54:52 +0900

    list

--- src/pages/HomeComp.jsx
+++ src/pages/HomeComp.jsx
@@ -1,7 +1,14 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 function HomeComp() {
-  return <div>HomeComp</div>;
+  return (
+    <div className="container mx-auto flex justify-center items-center h-screen">
+      <Link to="/book" className="bg-blue-500 text-white px-4 py-2 rounded-md">
+        책목록보기
+      </Link>
+    </div>
+  );
 }
 
 export default HomeComp;
